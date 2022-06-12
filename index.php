eee9af5169c69dfcba293acae752cb541d7cc528
Kirill L <thearrex@gmail.com>
2022-06-13 02:29:23 +0700

    init

--- index.php
+++ index.php
@@ -14,10 +14,9 @@ if ($text) {
         $reply = "Просто введи слово на немецком";
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
     } else {
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $text ]);
-//        $html = simplexml_load_file('https://dict.leo.org/dictQuery/m-vocab/rude/query.xml?lp=rude&lang=ru&search=' . $text . '&side=both&order=basic&partial=show&sectLenMax=16&n=1&filtered=-1&trigger=');
-//        $reply = $html->sectionlist->section[0]->entry[0]->side[0]->repr->small->i->m->t;
-//        $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
+        $html = simplexml_load_file('https://dict.leo.org/dictQuery/m-vocab/rude/query.xml?lp=rude&lang=ru&search=' . $text . '&side=both&order=basic&partial=show&sectLenMax=16&n=1&filtered=-1&trigger=');
+        $reply = $html->sectionlist->section[0]->entry[0]->side[0]->repr->small->i->m->t;
+        $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
     }
 } else {
     $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
