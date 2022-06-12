04bb698f966bb29bcf805eb8367d65c5174050c8
Kirill L <thearrex@gmail.com>
2022-06-12 18:31:47 +0700

    init

--- bot.php
+++ bot.php
@@ -1,20 +1,62 @@
 <?php
 
-use React\EventLoop\Loop;
-use \unreal4u\TelegramAPI\HttpClientRequestHandler;
-use \unreal4u\TelegramAPI\TgLog;
-use \unreal4u\TelegramAPI\Telegram\Methods\SendMessage;
+header('Content-Type: text/html; charset=utf-8'); // на всякий случай досообщим PHP, что все в кодировке UTF-8
 
-const BOT_TOKEN = '5578497960:AAERCgsXvpUoyDRSfhJ7tO_2WZ2CL3NVXDE';
-const A_USER_CHAT_ID = 241905674;
+$site_dir = dirname(dirname(__FILE__)).'/'; // корень сайта
+$bot_token = '1234567899:AAKJhkkjhkjhKhKhjkhkhk_kJhgkjhJhgjghjhG'; // токен вашего бота
+$data = file_get_contents('php://input'); // весь ввод перенаправляем в $data
+$data = json_decode($data, true); // декодируем json-закодированные-текстовые данные в PHP-массив
 
-$loop = Loop::get();
-$handler = new HttpClientRequestHandler($loop);
-$tgLog = new TgLog(BOT_TOKEN, $handler);
+// Для отладки, добавим запись полученных декодированных данных в файл message.txt,
+// который можно смотреть и понимать, что происходит при запросе к боту
+// Позже, когда все будет работать закомментируйте эту строку:
+file_put_contents(__DIR__ . '/message.txt', print_r($data, true));
 
-$sendMessage = new SendMessage();
-$sendMessage->chat_id = A_USER_CHAT_ID;
-$sendMessage->text = 'Hello world!';
+// Основной код: получаем сообщение, что юзер отправил боту и
+// заполняем переменные для дальнейшего использования
+if (!empty($data['message']['text'])) {
+    $chat_id = $data['message']['from']['id'];
+    $user_name = $data['message']['from']['username'];
+    $first_name = $data['message']['from']['first_name'];
+    $last_name = $data['message']['from']['last_name'];
+    $text = trim($data['message']['text']);
+    $text_array = explode(" ", $text);
 
-$tgLog->performApiRequest($sendMessage);
-$loop->run();
+    if ($text == '/help') {
+        $text_return = "Привет, $first_name $last_name, вот команды, что я понимаю: 
+/help - список команд
+/about - о нас
+";
+        message_to_telegram($bot_token, $chat_id, $text_return);
+    }
+    elseif ($text == '/about') {
+        $text_return = "verysimple_bot:
+Я пример самого простого бота для телеграм, написанного на простом PHP.
+Мой код можно скачивать, дополнять, исправлять. Код доступен в этой статье:
+https://www.novelsite.ru/kak-sozdat-prostogo-bota-dlya-telegram-na-php.html
+";
+        message_to_telegram($bot_token, $chat_id, $text_return);
+    }
+
+}
+
+// функция отправки сообщени в от бота в диалог с юзером
+function message_to_telegram($bot_token, $chat_id, $text, $reply_markup = '')
+{
+    $ch = curl_init();
+    $ch_post = [
+        CURLOPT_URL => 'https://api.telegram.org/bot' . $bot_token . '/sendMessage',
+        CURLOPT_POST => TRUE,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_TIMEOUT => 10,
+        CURLOPT_POSTFIELDS => [
+            'chat_id' => $chat_id,
+            'parse_mode' => 'HTML',
+            'text' => $text,
+            'reply_markup' => $reply_markup,
+        ]
+    ];
+
+    curl_setopt_array($ch, $ch_post);
+    curl_exec($ch);
+}
