2392a7b27721c5f8d9bf670c3d6bc983ab12df6d
Kirill L <thearrex@gmail.com>
2022-06-13 02:30:51 +0700

    init

--- index.php
+++ index.php
@@ -16,7 +16,9 @@ if ($text) {
     } else {
         $html = simplexml_load_file('https://dict.leo.org/dictQuery/m-vocab/rude/query.xml?lp=rude&lang=ru&search=' . $text . '&side=both&order=basic&partial=show&sectLenMax=16&n=1&filtered=-1&trigger=');
         $reply = $html->sectionlist->section[0]->entry[0]->side[0]->repr->small->i->m->t;
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
+        if ($reply) {
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
+        }
     }
 } else {
     $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
