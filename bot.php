c7c96b69a5503ed8ee1cd8e82059db9454f23515
Kirill L <thearrex@gmail.com>
2022-06-12 18:19:52 +0700

    init

--- bot.php
+++ bot.php
@@ -5,6 +5,9 @@ use \unreal4u\TelegramAPI\HttpClientRequestHandler;
 use \unreal4u\TelegramAPI\TgLog;
 use \unreal4u\TelegramAPI\Telegram\Methods\SendMessage;
 
+const BOT_TOKEN = '5578497960:AAERCgsXvpUoyDRSfhJ7tO_2WZ2CL3NVXDE';
+const A_USER_CHAT_ID = 241905674;
+
 $loop = Loop::get();
 $handler = new HttpClientRequestHandler($loop);
 $tgLog = new TgLog(BOT_TOKEN, $handler);
