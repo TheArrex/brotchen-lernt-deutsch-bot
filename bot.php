3d81807911b890a314d27c4ae176d50dc39f6ad9
Kirill L <thearrex@gmail.com>
2022-06-12 18:04:07 +0700

    init

---
+++ bot.php
@@ -0,0 +1,17 @@
+<?php
+
+use React\EventLoop\Loop;
+use \unreal4u\TelegramAPI\HttpClientRequestHandler;
+use \unreal4u\TelegramAPI\TgLog;
+use \unreal4u\TelegramAPI\Telegram\Methods\SendMessage;
+
+$loop = Loop::get();
+$handler = new HttpClientRequestHandler($loop);
+$tgLog = new TgLog(BOT_TOKEN, $handler);
+
+$sendMessage = new SendMessage();
+$sendMessage->chat_id = A_USER_CHAT_ID;
+$sendMessage->text = 'Hello world!';
+
+$tgLog->performApiRequest($sendMessage);
+$loop->run();
