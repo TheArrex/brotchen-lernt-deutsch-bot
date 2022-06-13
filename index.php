ab4132188d385d4b081ab6124f51d463d3353a00
Kirill L <thearrex@gmail.com>
2022-06-13 16:12:20 +0700

    genders with all entries

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
-include('vendor/autoload.php'); //Подключаем библиотеку
+include('vendor/autoload.php');
 use Telegram\Bot\Api;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
+$genders = [
+    'm' => 'мужской',
+    'f' => 'женский',
+    'n' => 'средний'
+];
+
 $log = new Logger('name');
 $log->pushHandler(new StreamHandler('php://stderr', Logger::WARNING));
 
-$telegram = new Api('5578497960:AAERCgsXvpUoyDRSfhJ7tO_2WZ2CL3NVXDE'); //Устанавливаем токен, полученный у BotFather
-$result = $telegram->getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+$telegram = new Api('5578497960:AAERCgsXvpUoyDRSfhJ7tO_2WZ2CL3NVXDE');
+$result = $telegram->getWebhookUpdates();
 
-$text = $result["message"]["text"]; //Текст сообщения
-$chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
-$name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+$text = $result["message"]["text"];
+$chat_id = $result["message"]["chat"]["id"];
 
 if ($text) {
     if ($text == "/start") {
@@ -23,20 +28,18 @@ if ($text) {
         $html = simplexml_load_file('https://dict.leo.org/dictQuery/m-vocab/rude/query.xml?lp=rude&lang=ru&search=' . $text . '&side=both&order=basic&partial=show&sectLenMax=16&n=1&filtered=-1&trigger=');
         if ($html) {
             if ($html->sectionlist->section[0]) {
-                $n = $html->sectionlist->section[0]->entry[0]->side[0]->repr->small->i->m->t;
-                foreach ($html->sectionlist->section[0]->entry[0]->side[0]->words->word as $word) {
-                    $reply .= "<b>" . $word . "</b>\n\n";
+                foreach ($html->sectionlist->section[0]->entry as $entry) {
+                    $n = $entry->side[0]->repr->small->i->m->t;
+                    foreach ($entry->side[0]->words->word as $word) {
+                        $reply .= "<b>" . $word . "</b>\n\n";
 
-                    if ($word->attributes()->implicit_mf) {
-                        $reply .= "Род: " . $word->attributes()->implicit_mf . "\n\n";
-                    } else {
-                        $reply .= 'Грамматическое число: ' . $n;
+                        if ($word->attributes()->implicit_mf) {
+                            $reply .= "Род: " . $genders[$word->attributes()->implicit_mf] . "\n\n";
+                        } else {
+                            $reply .= 'Грамматическое число: ' . $n;
+                        }
                     }
                 }
-
-//            if ($n->attributes()->n == 'grm_pl') {
-//
-//            }
             } else {
                 $reply .= 'Булочка, ты опечаталась';
             }
