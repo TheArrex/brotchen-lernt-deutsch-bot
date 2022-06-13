4a548c8a7fcc6a4ac22f9ef3cbadfe6c569f38f1
Kirill L <thearrex@gmail.com>
2022-06-13 18:37:45 +0700

    indikatives

--- index.php
+++ index.php
@@ -47,7 +47,6 @@ if ($text) {
                                     }
                                     break;
                                 case 'verb':
-                                    $reply .= $entryAttributes->url;
                                     $stemming = simplexml_load_file('https://dict.leo.org/dictQuery/m-vocab/rude/stemming.xml' . $entryAttributes->url . '&onlyLoc=result');
 
                                     if ($stemming) {
